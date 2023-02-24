54274d01c28d7a83bb4e755d4f5a59a222d88f9c
Ayhan <ayhan.durukan25@gmail.com>
2023-02-25 00:00:44 +0100

    Methode pour lister et cloner en cours

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        SourceDirectory D = new SourceDirectory("C:/Users/ayhan/OneDrive/Documents/1er année ENSISA/Semestre 2/AOO Java/CloneDirectory/test");
-        D.listDirectory();
+    public static void main(String[] args) throws IOException {
+        SourceDirectory D = new SourceDirectory("C:/Users/ayhan/OneDrive/Documents/1er année ENSISA/Semestre 2/AOO Java/CloneDirectory/test","C:/Users/ayhan/OneDrive/Documents/1er année ENSISA/Semestre 2/AOO Java/CloneDirectory");
+        D.listDirectory(D.getDirectory());
     }
 }
--- src/SourceDirectory.java
+++ src/SourceDirectory.java
@@ -1,30 +1,51 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SourceDirectory {
     private String path;
     private File directory;
-
-    public SourceDirectory (String path) {
+    private String remotePath;
+    private File remotedirectory;
+    public SourceDirectory (String path, String remotePath) {
         this.path = path;
         this.directory = new File(path);
+        this.remotePath = remotePath;
+        this.remotedirectory = new File(remotePath);
     }
 
-    public void listDirectory () {
-        List<File> files = new ArrayList<>();
+    public void listDirectory (File directory) throws IOException {
+        List<String> files = new ArrayList<>();
         if (directory.isDirectory()) {
             File[] directoryFiles = directory.listFiles();
             if (directoryFiles != null) {
                 for (File file : directoryFiles) {
                     if (file.isFile()) {
-                        files.add(file);
-                        System.out.println(file.getName());
-                    }
+                        files.add(file.getName());
+                        System.out.println(equals(path));
+                        System.out.println(path);
+                        System.out.println(file.getParent());
+                        if (file.getParent().equals(path)) {
+                            System.out.println("On est la");
+                            System.out.println(file.getName());
+                        }else {
+                            File modifiedPaths = new File(file.getParent());
+                            System.out.println(modifiedPaths.getName() + "/" + file.getName());
+                        }
+                    } else if (file.isDirectory())
+                        listDirectory(file);
                 }
             }
         }
-
+        //copy(files);
     }
 
+    private void copy (List<File> files) throws IOException {
+        DestinationDirectory remoteDir = new DestinationDirectory(files,this.remotePath);
+    }
+
+    public File getDirectory() {
+        return directory;
+    }
 }
